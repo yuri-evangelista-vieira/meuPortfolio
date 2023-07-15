234ab7b4c17810c6648d6e7b6e9b8c9c9d1d7cb8
yurigba810 <yurigba810@gmail.com>
2023-07-15 03:53:32 -0300

    2 atualizações Título da página foi alterado. Agora para "Yuri Evangelista" Cabecalho.jsx foi modificado junto com o cabecalho.css. Agora a página tem a estrutura que eu quero que tenha.

--- meuportfolio/src/componentes/cabecalho.jsx
+++ meuportfolio/src/componentes/cabecalho.jsx
@@ -9,21 +9,32 @@ function Cabecalho() {
     return (
         <>
             <div className="divBox">
-                <h1>Olá, eu sou o Yuri</h1>
+                <div id="main-section">
 
+                <div id="jobTitulo">
 
-                <a href="https://www.linkedin.com/in/yuri-evangelista/" target="_blank">
-                    <img id="imgLinkedin" src={ImgLinkedin} alt="Meu Perfil no Linkedin" />
-                </a>
-                <a href="https://github.com/yuri-evangelista-vieira" target="_blank">
-                    <img src={ImgGithub} alt="Meu Perfil no GitHub" />
-                </a>
-                <a href="C:\Users\Yuri Evangelista\Documents/curriculoYuri.pdf" download>
-                    <img src={ImgCurriculo} alt="Baixe o meu currículo"/>
-                </a>
+                    <div id="apresentacao">
+                        <h2>Olá, eu sou o Yuri</h2>
+                    </div>
 
+                    <h1>Front-end</h1>
+                    <h1>Developer</h1>
+                </div>
 
-                <h1>Front-end Developer</h1>
+                    <div id="linkIcones">
+                        <a href="https://www.linkedin.com/in/yuri-evangelista/" target="_blank">
+                            <img src={ImgLinkedin} alt="Meu Perfil no Linkedin" />
+                        </a>
+                        <a href="https://github.com/yuri-evangelista-vieira" target="_blank">
+                            <img src={ImgGithub} alt="Meu Perfil no GitHub" />
+                        </a>
+                        <a href="C:\Users\Yuri Evangelista\Documents/curriculoYuri.pdf" download>
+                            <img src={ImgCurriculo} alt="Baixe o meu currículo" />
+                        </a>
+                    </div>
+
+                </div>
+
             </div>
         </>
     )
